f6877ad0ff8f6f66340bc2b0dc0e1060a43c60ad
Sulton Elhadi <elhadi.sulton@gmail.com>
2016-05-06 13:55:53 -0400

    whatever helpful message you want to have here

--- src/main/java/tiy/webapp/TiyBankingSystemApplication.java
+++ src/main/java/tiy/webapp/TiyBankingSystemApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TiyBankingSystemApplication {
+	//Just making sure itdont run
 
 	public static void main(String[] args) {
 		SpringApplication.run(TiyBankingSystemApplication.class, args);
